dd6b26c2e829692202a26f7033f4932a3843a28a
Aditya Gupta <divyashgupta2005@gmail.com>
2021-09-08 22:22:59 +0530

    🔎Added simple search feature

---
+++ app.js
@@ -0,0 +1,47 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      search: '',
+      books: [
+        {
+          genre: 'Epilogue',
+          title: "Or of her there before.",
+          description:
+            "Da noi maesta suoi nel cosa il e in esse. Mortale trapassare delle giudicio nome dare forse, da pieno pieno.",
+          art: "https://picsum.photos/526/354",
+        },
+        {
+          genre: 'Biography',
+          title: "Herzen steigt versuch unbestimmten.",
+          description:
+            "Von von träume ich gerne oft helle du, der komm schwester ort gedanken, verwundert mund ort gefärbt ich ja dich,.",
+          art: "https://picsum.photos/527/354",
+        },
+        {
+          genre: 'Biography',
+          title: "Herzen steigt versuch unbestimmten.",
+          description:
+            "Von von träume ich gerne oft helle du, der komm schwester ort gedanken, verwundert mund ort gefärbt ich ja dich,.",
+          art: "https://picsum.photos/527/354",
+        },
+        {
+          genre: 'Biography',
+          title: "Herzen steigt versuch unbestimmten.",
+          description:
+            "Von von träume ich gerne oft helle du, der komm schwester ort gedanken, verwundert mund ort gefärbt ich ja dich,.",
+          art: "https://picsum.photos/527/354",
+        },
+        {
+          genre: 'Biography',
+          title: "Herzen steigt versuch unbestimmten.",
+          description:
+            "Von von träume ich gerne oft helle du, der komm schwester ort gedanken, verwundert mund ort gefärbt ich ja dich,.",
+          art: "https://picsum.photos/527/354",
+        },
+      ],
+    };
+  },
+});
+
+app.mount("#app");
+
